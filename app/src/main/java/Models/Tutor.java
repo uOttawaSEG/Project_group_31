9d5b0e8a486d92f084f86f38c03e178eafc8a888
eelsaigh <139486859+eelsaigh@users.noreply.github.com>
2025-09-28 14:43:59 -0400

    Initial Commit Tutor Class with first name

---
+++ app/src/main/java/Models/Tutor.java
@@ -0,0 +1,20 @@
+public class Tutor {
+
+    
+    private String firstName;
+
+    // Constructor to create a Tutor object with first name
+    public Tutor(String firstName) {
+        this.firstName = firstName;
+    }
+
+    // Get the first name
+    public String getFirstName() {
+        return firstName;
+    }
+
+    //Set or update the first name
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
