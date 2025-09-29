e9d588fc7d19f139e029fb8d0451ba30481f0b81
DuyLe250 <vietduy2507@gmail.com>
2025-09-29 16:36:42 -0400

    commit with last name ( 3rd comments)...

--- app/src/main/java/Models/Student.java
+++ app/src/main/java/Models/Student.java
@@ -1,14 +1,26 @@
+
 public class Student {
     private String firstName;
+    private String lastName;
+
+    //Constructor to create student object with first name and last name
+    public Student(String firstName, String lastName) {
 
         this.firstName = firstName;
+        this.lastName = lastName;
     }
     //Get the first name
     public String getFirstName() {
-        return firstName;
-    }
-    //Set or update the first name
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-}}
+        public class Student {
+            public void setFirstName(String firstName) {
+                this.firstName = firstName;
+            }
+            // Get the last name
+            public String getLastName() {
+                return lastName;
+            }
+            // Set or update the last name
+            public void setLastName(String lastName) {
+                this.lastName = lastName;
+            }
+        }
