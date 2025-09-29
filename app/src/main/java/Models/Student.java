9e6dd968042a3008ed021745a58b4ce6216c1183
fareshelal <fhela021@uottawa.ca>
2025-09-29 17:26:27 -0400

    Creating User class containing everything in common

--- app/src/main/java/Models/Student.java
+++ app/src/main/java/Models/Student.java
@@ -1,27 +1,25 @@
 
-public class Student {
+public class User {
     private String firstName;
     private String lastName;
     private String email;  //Username
     private String password;
     private String phoneNumber;
-    private String programOfStudy;
+
 
     //Constructor to create student object with first name and last name
-    public Student(String firstName, String lastName, String email, String password, String phoneNumber, String programOfStudy) {
+    public User(String firstName, String lastName, String email, String password, String phoneNumber, String programOfStudy) {
 
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.password = password;
         this.phoneNumber = phoneNumber;
-        this.programOfStudy = programOfStudy;
     }
     //Get the first name
     public String getFirstName() {
-
+        return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
@@ -33,4 +31,36 @@ public class Student {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @override
+    public String toString(){
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+
+
+
+    }
 }
+
