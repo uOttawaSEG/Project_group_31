aebdbfac4f444f77c21e4a5df0dc3f08caaac39d
fareshelal <fhela021@uottawa.ca>
2025-09-29 17:02:50 -0400

    working on student class

--- app/src/main/java/Models/Student.java
+++ app/src/main/java/Models/Student.java
@@ -2,25 +2,35 @@
 public class Student {
     private String firstName;
     private String lastName;
+    private String email;  //Username
+    private String password;
+    private String phoneNumber;
+    private String programOfStudy;
 
     //Constructor to create student object with first name and last name
-    public Student(String firstName, String lastName) {
+    public Student(String firstName, String lastName, String email, String password, String phoneNumber, String programOfStudy) {
 
         this.firstName = firstName;
         this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.programOfStudy = programOfStudy;
     }
     //Get the first name
     public String getFirstName() {
-        public class Student {
-            public void setFirstName(String firstName) {
-                this.firstName = firstName;
-            }
-            // Get the last name
-            public String getLastName() {
-                return lastName;
-            }
-            // Set or update the last name
-            public void setLastName(String lastName) {
-                this.lastName = lastName;
-            }
-        }
+
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    // Get the last name
+    public String getLastName() {
+        return lastName;
+    }
+    // Set or update the last name
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
