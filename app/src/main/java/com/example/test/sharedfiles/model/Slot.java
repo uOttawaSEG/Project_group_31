5bf9d96cb58704ce18413446838578c0e21af25c
Mrc980 <34220790+Mrc980@users.noreply.github.com>
2025-11-08 21:19:42 -0500

    Added Slot.java to model package

---
+++ app/src/main/java/com/example/test/sharedfiles/model/Slot.java
@@ -0,0 +1,31 @@
+package com.example.test.sharedfiles.model;
+
+import java.time.LocalDate;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+public class Slot {
+    private String tutorId;
+    private String date;
+    private String startTime;
+    private String endTime;
+    private boolean requiresApproval;
+    private boolean isAvailable;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
