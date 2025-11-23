bb996ee7604bb0dbc13b32909dae1df5c1524e3c
Mrc980 <34220790+Mrc980@users.noreply.github.com>
2025-11-22 20:06:24 -0500

    Fixed and Implemented BookingTest

--- app/src/test/java/com/example/test/BookingTest.java
+++ app/src/test/java/com/example/test/BookingTest.java
@@ -1,15 +1,31 @@
 package com.example.test;
-import org.junit.Test;
 
-import java.time.LocalDateTime;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BookingTest {
 
-    public void canCancel() {
-        LocalDateTime localDateTime = LocalDateTime.of(2025, 11, 22, 12, 0);
-
+    private boolean canCancel(long sessionStart, long now) {
+        long sessionDifference = sessionStart - now;
+        long hours = sessionDifference / (1000L * 60L * 60L);
+        return hours >= 24;
     }
 
+    public void testCancel() {
+
+        long now = 0L;
+
+        // Session starting less than 24 hours
+        long start5Hours = 5L * 60L * 60L * 1000L;
+
+        // Session starting more than 24 hours
+        long start30Hours = 30L * 60L * 60L * 1000L;
+
+        // Asserts that sessions starting in less than 24 hours can't be cancelled
+        assertFalse("returns false", canCancel(start5Hours, now));
+
+        // Asserts that sessions starting in less than or equal to 24 hours can be cancelled
+        assertTrue("return true ", canCancel(start30Hours, now));
+    }
 }
