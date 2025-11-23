5814ca6c5ca15fbb6b65d6d8b629bd85daa73891
Mrc980 <34220790+Mrc980@users.noreply.github.com>
2025-11-22 19:25:39 -0500

    Created BookingTest.java

---
+++ app/src/test/java/com/example/test/BookingTest.java
@@ -0,0 +1,15 @@
+package com.example.test;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.*;
+
+public class BookingTest {
+
+    public void canCancel() {
+        LocalDateTime localDateTime = LocalDateTime.of(2025, 11, 22, 12, 0);
+
+    }
+
+}
